bb4d6923f15ec63d364e719cc27b7a576184c8db
Kiril Vatev <vatev.1@gmail.com>
2016-07-22 17:53:38 -0400

    starting to add support for non-windows envs

--- node/open-shell-domain.js
+++ node/open-shell-domain.js
@@ -4,14 +4,41 @@
     var fs = require('fs');
     var spawn = require('child_process').spawn;
 
+    function defaultShell() {
+        // code in this function adapted from Sindre Sorhus
+        // MIT License
+        // https://github.com/sindresorhus/default-shell/blob/d3e441382f0b95515dd208ca228ace817a3535a2/index.js
+        var env = process.env;
+
+        if (process.platform === 'darwin') {
+            return env.SHELL || '/bin/bash';
+        }
+
+        if (process.platform === 'win32') {
+            return env.COMSPEC || 'cmd.exe';
+        }
+
+        return env.SHELL || '/bin/sh';
+    }
+    
+    function shellArgs(path) {
+        if (/^win/.test(process.platform)) {
+            return ['/c', 'start', 'cmd', '/K', 'cd', '/d', path];
+        } else {
+            // TODO
+            return [];
+        }
+    }
+    
     function cmdStartTerm(path, term) {
         var stdio = (function() {
             if (!/^win/.test(process.platform)) {
                 return 'ignore';
             }
 
-            // believe it or not, Windows does have a null device
-            // that is not the nul file
+            // Believe it or not, Windows does have a null device
+            // that is not the nul file. We will use it here to
+            // make sure the spawned process detaches correctly.
             var NULLDEV = '\\\\.\\NUL';
             var MODE = {
                 READ: 'r',
@@ -24,8 +51,10 @@
                 fs.openSync(NULLDEV, MODE.WRITE)
             ];
         }());
+        
+        var shell = defaultShell();
 
-        spawn('cmd.exe', ['/c', 'start', 'cmd', '/K', 'cd', '/d', path], {
+        spawn(shell, shellArgs(path), {
             detached: true,
             stdio: stdio
         }).unref();
