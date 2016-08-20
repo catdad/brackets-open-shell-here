303001e195c0f5eaf7b6160068343fccb4954345
Kiril Vatev <vatev.1@gmail.com>
2016-08-19 21:03:23 -0400

    really awful build for cl
    #3

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@
 
 var util = require('util');
 var path = require('path');
+var os = require('os');
+
+var gulp = require('gulp');
+var async = require('async');
+var del = require('del');
+var zip = require('gulp-zip');
+var shellton = require('shellton');
 
 var pkg = require('./package.json');
 
-var gulp = require('gulp');
-var del = require('del');
-var zip = require('gulp-zip');
-
-var SOURCE = ['main.js', 'node/**', 'bin/**', 'style/**'];
+var SOURCE = ['main.js', 'node/**', 'bin/*.exe', 'style/**'];
 var DEST = 'output';
 
 gulp.task('clean', function() {
@@ -22,4 +25,102 @@ gulp.task('zip', function() {
     gulp.src(SOURCE, { base: path.resolve(__dirname) })
         .pipe(zip(filename))
         .pipe(gulp.dest(DEST));
+});
+
+gulp.task('compile', function(done) {
+    var tools = {};
+    var regex = /^vs([0-9]{3})comntools$/i;
+    
+//    var comtools = process.env.VS140COMNTOOLS;
+//    var comtools = process.env.VS120COMNTOOLS;
+//    var comtools = process.env.VS100COMNTOOLS;
+    
+    Object.keys(process.env).forEach(function(key) {
+        if (regex.test(key)) {
+            var match = key.match(regex);
+            var version = match[1];
+            
+            tools[version] = {
+                version: version,
+                envvar: key,
+                path: process.env[key]
+            };
+        }
+    });
+    
+    var versions = Object.keys(tools).sort(function (a, b) {
+        return +b - +a;
+    });
+    
+    if (!versions.length) {
+        throw new Error('did not find any common tools');
+    }
+    
+    var ver = versions[0];
+    var opts = {
+        varsscript: path.join(tools[ver].path, 'vsvars32.bat')
+    };
+    
+    async.series([
+        function getPaths(next) {
+            var task = util.format('"%s" && where cl', opts.varsscript);
+
+            shellton.exec({
+                task: task
+            }, function(err, stdout, stderr) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (stderr.trim()) {
+                    return next(stderr);
+                }
+
+                if (stdout.trim().length === 0) {
+                    return next('nothing in stdout');
+                }
+
+                var lines = stdout.split(os.EOL);
+
+                if (!lines.length) {
+                    return next('no lines found in stdout');
+                }
+
+                opts.cl = lines[0];
+
+                next();
+            });
+        },
+        function execBuild(next) {
+            var SRC = path.join('native', 'open.c');
+            var OUT = path.join('bin', 'open.exe');
+            var OBJ = path.join('bin', 'open.obj');
+    
+            var task = util.format(
+                '"%s" && "%s" %s /Fe:%s /Fo:%s',
+                opts.varsscript,
+                opts.cl,
+                SRC,
+                OUT,
+                OBJ
+            );
+            
+            console.log('running:', task);
+            
+            shellton.exec({
+                task: task,
+                cwd: __dirname
+            }, function(err, stdout, stderr) {
+                console.log(stdout);
+                console.log('--------------');
+                console.log(stderr);
+            });
+        }
+    ], function(err) {
+        if (err) {
+            return done(err);
+        }
+        
+        done();
+    });
 });
