5f6952c8fd179118eb88701e373cd3f42f5e9628
Kiril Vatev <vatev.1@gmail.com>
2016-11-29 19:37:17 -0500

    adding title to the windows command prompt

--- node/win-brackets-1.8.js
+++ node/win-brackets-1.8.js
@@ -1,10 +1,13 @@
 /* jslint node: true */
 
+var path = require('path');
+var util = require('util');
 var spawn = require('child_process').spawn;
 
 module.exports = function openShellWindows(dirpath) {
+    var title = util.format('"%s"', path.basename(dirpath));
 
-    spawn('cmd.exe', ['/c', 'start', 'cmd.exe'], {
+    spawn('cmd.exe', ['/c', 'start', title, 'cmd.exe'], {
         stdio: 'ignore',
         cwd: dirpath,
         windowsVerbatimArguments: true
