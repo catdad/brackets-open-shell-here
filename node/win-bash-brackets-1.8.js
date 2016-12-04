2ec247091d7f84b621c94ca5dc4fa65281dcdb64
Kiril Vatev <vatev.1@gmail.com>
2016-12-04 14:56:46 -0500

    moar es6

--- node/win-bash-brackets-1.8.js
+++ node/win-bash-brackets-1.8.js
@@ -1,10 +1,9 @@
-/* jslint node: true */
+/* jslint node: true, esversion: 6 */
 
-var util = require('util');
 var spawn = require('child_process').spawn;
 
 module.exports = function openShellWindows(dirpath, title) {
-    var ttl = util.format('"%s"', title);
+    var ttl = `"${title}"`;
 
     spawn('cmd.exe', ['/c', 'start', ttl, 'bash'], {
         stdio: 'ignore',
