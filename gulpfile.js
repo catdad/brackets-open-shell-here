4108821147c6cc0487def612754ccdbae34b52c3
Kiril Vatev <vatev.1@gmail.com>
2016-12-18 12:00:33 -0500

    removing old windows build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,15 @@
 
 var util = require('util');
 var path = require('path');
-var fs = require('fs');
 
 var gulp = require('gulp');
-var gutil = require('gulp-util');
-var async = require('async');
-var mkdirp = require('mkdirp');
 var del = require('del');
 var zip = require('gulp-zip');
 var sequence = require('gulp-sequence');
-var shellton = require('shellton');
 
 var pkg = require('./package.json');
 
-var SOURCE = ['main.js', 'package.json', 'node/**', 'bin/*.exe', 'style/**'];
+var SOURCE = ['main.js', 'package.json', 'node/**', 'style/**'];
 var DEST = 'output';
 
 gulp.task('clean', function() {
@@ -34,82 +29,4 @@ gulp.task('zip', function() {
         .pipe(gulp.dest(DEST));
 });
 
-gulp.task('compile', ['clean:bin'], function(done) {
-    var regex = /microsoft visual studio ([0-9]+\.[0-9])+/i;
-    var opts = {};
-
-    async.series([
-        function makeBin(next) {
-            mkdirp('bin', next);
-        },
-        function getMsvs(next) {
-            var programFiles = process.env['ProgramFiles(x86)'] || process.env.ProgramFiles;
-
-            if (!programFiles) {
-                return setImmediate(next, new Error('could not find ProgramFiles'));
-            }
-
-            fs.readdir(programFiles, function(err, list) {
-                if (err) {
-                    return next(err);
-                }
-
-                var msvsVersions = list.filter(function(item) {
-                    return regex.test(item);
-                }).map(function(item) {
-
-                    var root = path.resolve(programFiles, item);
-
-                    return {
-                        path: root,
-                        version: item.match(regex)[1],
-                        vcvars: path.resolve(root, 'VC', 'vcvarsall.bat'),
-                        cl: path.resolve(root, 'VC', 'bin', 'cl.exe')
-                    };
-                }).sort(function(a, b) {
-                    return Number(b.version) - Number(a.version);
-                });
-
-                opts = msvsVersions[0];
-
-                next();
-            });
-        },
-        function execBuild(next) {
-            var SRC = path.join('native', 'open.c');
-            var OUT = path.join('bin', 'open.exe');
-            var OBJ = path.join('bin', 'open.obj');
-
-            var task = util.format(
-                '"%s" x86 && "%s" "%s" /Fe:%s /Fo:%s',
-                opts.vcvars,
-                opts.cl,
-                SRC,
-                OUT,
-                OBJ
-            );
-
-            gutil.log('running: \'%s\'', gutil.colors.cyan(task));
-
-            shellton.exec({
-                task: task,
-                cwd: __dirname
-            }, function(err, stdout, stderr) {
-
-                console.log(gutil.colors.yellow(stdout.trim()));
-                console.log('--------------');
-                console.log(gutil.colors.red(stderr.trim()));
-
-                next();
-            });
-        }
-    ], function(err) {
-        if (err) {
-            return done(err);
-        }
-
-        done();
-    });
-});
-
-gulp.task('default', sequence('clean', 'compile', 'zip'));
+gulp.task('default', sequence('clean', 'zip'));
