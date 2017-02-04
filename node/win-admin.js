0a02d4b0349a79c358a208e03b6c548026906c90
Kiril Vatev <vatev.1@gmail.com>
2017-02-04 12:55:54 -0500

    adding comments

--- node/win-admin.js
+++ node/win-admin.js
@@ -5,6 +5,8 @@ var spawn = require('child_process').spawn;
 module.exports = function (type) {
     return function openShellWindows(dirpath, title) {
 
+        // so... use powershell to run cmd as admin, and then use that cmd
+        // to launch the requested shell in the correct directory
         var cmd = `powershell.exe -Command "Start-Process cmd \\"/c start /d %cd% ${type}\\" -Verb RunAs"`;
 
         spawn('cmd.exe', ['/c', cmd], {
