30effe355f5bcfff19ae4cc61cc20845c467c50c
Kiril Vatev <vatev.1@gmail.com>
2017-02-11 16:32:26 -0500

    adding a test for initializing the node domain

--- test/open-shell-domain.test.js
+++ test/open-shell-domain.test.js
@@ -4,10 +4,42 @@ var expect = require('chai').expect;
 
 var domain = require('../node/open-shell-domain.js');
 
+function fakeManager() {
+    var commands = {};
+
+    return {
+        hasDomain: () => true,
+        registerCommand: (extension, name, func, isAsync, description, parameters, returns) => {
+            commands[name] = { extension, name, func, isAsync, description, parameters, returns };
+        },
+        _getCommands: () => commands
+    };
+}
+
 describe('[open-shell-domain]', () => {
     it('exports an object with an init method', () => {
         expect(domain).to.be.an('object')
             .and.to.have.property('init')
             .and.to.be.a('function');
+    });
+
+    describe('#init', () => {
+        it('registers 2 commands', () => {
+            var manager = fakeManager();
+
+            domain.init(manager);
+
+            var commands = manager._getCommands();
+
+            expect(Object.keys(commands)).to.deep.equal(['start', 'getSupported']);
+        });
+    });
+
+    describe('"start" command', () => {
+
+    });
+
+    describe('"getSupported" command', () => {
+
     });
 });
