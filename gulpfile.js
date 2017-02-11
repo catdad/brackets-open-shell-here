ada0f7e90ece171a2b9717dad8a2dc5be2932521
Kiril Vatev <vatev.1@gmail.com>
2017-02-11 15:25:04 -0500

    adding code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var del = require('del');
 var zip = require('gulp-zip');
 var sequence = require('gulp-sequence');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var filter = require('gulp-filter');
 
 var pkg = require('./package.json');
 
@@ -23,11 +25,11 @@ var source = (function () {
 
 var DEST = 'output';
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return del(DEST);
 });
 
-gulp.task('zip', function() {
+gulp.task('zip', function () {
     var filename = util.format('%s.zip', pkg.name);
 
     gulp.src(source.lib, { base: path.resolve(__dirname) })
@@ -35,12 +37,24 @@ gulp.task('zip', function() {
         .pipe(gulp.dest(DEST));
 });
 
+gulp.task('coverage-instrument', function () {
+    return gulp.src(source.lib)
+        .pipe(filter(['**/*.js']))
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage-report', function () {
+    return gulp.src(source.test)
+        .pipe(istanbul.writeReports());
+});
+
 gulp.task('mocha', function () {
     return gulp.src(source.test)
         .pipe(mocha());
 });
 
-gulp.task('test', ['mocha']);
+gulp.task('test', sequence('coverage-instrument', 'mocha', 'coverage-report'));
 
 gulp.task('build', sequence('clean', 'zip'));
 
