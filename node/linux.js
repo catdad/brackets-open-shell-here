e680d62d844159d52143a231065911b0a3a7bc9a
Kiril Vatev <vatev.1@gmail.com>
2017-02-12 12:36:52 -0500

    adding error handling on the spawn processes

--- node/linux.js
+++ node/linux.js
@@ -1,12 +1,12 @@
 /* jslint node: true, esversion: 6 */
 
-var spawn = require('child_process').spawn;
+var spawn = require('./spawn.js');
 
 module.exports = function (type) {
     return function openShellWindows(dirpath, title) {
         spawn(type, {
             stdio: 'ignore',
             cwd: dirpath
-        }).unref();
+        });
     };
 };
