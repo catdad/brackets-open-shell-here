f1ee6e52a1dbb58cc7c7e7eb54969866b42b4e5f
Kiril Vatev <vatev.1@gmail.com>
2017-02-13 21:11:21 -0500

    more spawn tests, using fakeIo to test console output

--- test/spawn.test.js
+++ test/spawn.test.js
@@ -8,20 +8,96 @@ var platform = /^win/.test(process.platform) ? 'win' : 'linux';
 var bin = platform === 'win' ? 'cmd.exe' : 'bash';
 var arg = platform === 'win' ? '/c' : '-c';
 
+function getSpawn() {
+    return spawn(bin, [arg, 'echo', 'pineapples']);
+}
+
+// allow hijacking IO, so that we can both
+// test values and not log during a test
+var fakeIo = (function () {
+
+    var originalStdout = process.stdout.write;
+    var originalStderr = process.stderr.write;
+
+    var outData = [];
+    var errData = [];
+
+    function collect(arr) {
+        return function (val) {
+            arr.push(new Buffer(val));
+        };
+    }
+
+    return {
+        activate: function () {
+            process.stdout.write = collect(outData);
+            process.stderr.write = collect(errData);
+        },
+        deactivate: function () {
+            process.stdout.write = originalStdout;
+            process.stderr.write = originalStderr;
+
+            var data = {
+                stdout: Buffer.concat(outData).toString(),
+                stderr: Buffer.concat(errData).toString()
+            };
+
+            outData = [];
+            errData = [];
+
+            return data;
+        }
+    };
+})();
+
 describe('[spawn]', () => {
+
     it('spawns real processes', function (done) {
-        var proc = spawn(bin, [arg, 'echo', 'pineapples']);
+        fakeIo.activate();
+
+        var proc = getSpawn();
 
         proc.on('error', done);
 
         proc.on('exit', function (code) {
+            var ioData = fakeIo.deactivate();
+
             expect(code).to.equal(0);
+
+            expect(ioData.stdout).to.match(/shell launcher exited with code/);
+            expect(ioData.stderr).to.have.lengthOf(0);
 
             done();
         });
     });
 
-    it('adds an error handler to the process');
+    it('adds an error handler to the process', function (done) {
+        fakeIo.activate();
 
-    it('adds an exit handler on the process');
+        var proc = getSpawn();
+
+        expect(proc.listenerCount('error')).to.equal(1);
+
+        proc.on('exit', () => {
+            fakeIo.deactivate();
+
+            done();
+        });
+        proc.on('error', done);
+    });
+
+    it('adds an exit handler on the process', function (done) {
+        fakeIo.activate();
+
+        var proc = getSpawn();
+
+        expect(proc.listenerCount('exit')).to.equal(1);
+
+        proc.on('exit', () => {
+            fakeIo.deactivate();
+
+            done();
+        });
+        proc.on('error', done);
+    });
 });
