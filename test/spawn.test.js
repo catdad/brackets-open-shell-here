de8959c4cb1df3023903ea6208c44a51d229db2d
Kiril Vatev <vatev.1@gmail.com>
2017-02-13 19:04:46 -0500

    adding a spawn test

---
+++ test/spawn.test.js
@@ -0,0 +1,23 @@
+/* jshint node: true, mocha: true, esversion: 6 */
+
+var expect = require('chai').expect;
+
+var spawn = require('../node/spawn.js');
+
+var platform = /^win/.test(process.platform) ? 'win' : 'linux';
+var bin = platform === 'win' ? 'cmd.exe' : 'bash';
+var arg = platform === 'win' ? '/c' : '-c';
+
+describe('[spawn]', () => {
+    it('spawns real processes', function (done) {
+        var proc = spawn(bin, [arg, 'echo', 'pineapples']);
+
+        proc.on('error', done);
+
+        proc.on('exit', function (code) {
+            expect(code).to.equal(0);
+
+            done();
+        });
+    });
+});
