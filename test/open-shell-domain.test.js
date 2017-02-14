3ed4565c29e41b2c0f9eef45afdb9d20ee8efcb5
Kiril Vatev <vatev.1@gmail.com>
2017-02-14 18:07:35 -0500

    adding start tests to the domain

--- test/open-shell-domain.test.js
+++ test/open-shell-domain.test.js
@@ -1,10 +1,18 @@
 /* jshint node: true, mocha: true, esversion: 6 */
 
+var path = require('path');
+
 var expect = require('chai').expect;
 
 var tools = require('./common-launcher-tests.js');
 
-var domain = require('../node/open-shell-domain.js');
+var domain = require.resolve('../node/open-shell-domain.js');
+
+var os = (/^win/.test(process.platform) ? 'win' : 'linux');
+
+function getDomain() {
+    return require(domain);
+}
 
 function fakeManager() {
     var commands = {};
@@ -21,7 +29,7 @@ function fakeManager() {
 function getCommands() {
     var manager = fakeManager();
 
-    domain.init(manager);
+    getDomain().init(manager);
 
     return manager._getCommands();
 }
@@ -34,36 +42,45 @@ function getCommand(name) {
 
 describe('[open-shell-domain]', () => {
     it('exports an object with an init method', () => {
-        expect(domain).to.be.an('object')
+        expect(getDomain()).to.be.an('object')
             .and.to.have.property('init')
             .and.to.be.a('function');
     });
 
     describe('#init', () => {
         it('registers 2 commands', () => {
-            var manager = fakeManager();
-
-            domain.init(manager);
-
-            var commands = manager._getCommands();
+            var commands = getCommands();
 
             expect(Object.keys(commands)).to.deep.equal(['start', 'getSupported']);
         });
     });
 
-    describe('"openShell" command', function () {
-        var COMMAND = 'openShell';
+    describe('"start" command', function () {
+        var COMMAND = 'start';
+        var PATH = path.resolve('.');
 
-        var shells = [
-            'win-default',
-            'win-bash',
-            'win-powershell',
-            'linux-default',
-            'linux-xfce4-terminal'
-        ];
+        var shells = ['default'].concat(
+            os === 'win' ?
+            ['bash', 'powershell'] :
+            ['xfce4-terminal']
+        );
 
         shells.forEach(function (name) {
-            it(`can open ${name}`);
+            it(`can open ${name}`, function (done) {
+                tools.enableMock(function () {
+                    // all we really care about is that this is called
+
+                    expect(arguments).to.have.length.above(0);
+
+                    tools.disableMock();
+
+                    done();
+                });
+
+                var command = getCommand(COMMAND);
+
+                command.func(PATH, name);
+            });
         });
     });
 });
