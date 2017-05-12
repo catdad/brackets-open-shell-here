fef51b2cbc212de1b9583aeacdcede4111f3925a
Kiril Vatev <vatev.1@gmail.com>
2017-05-12 17:13:49 -0400

    usign mock-stdio to capture logging in tests
    #20

--- test/spawn.test.js
+++ test/spawn.test.js
@@ -1,6 +1,7 @@
 /* jshint node: true, mocha: true, esversion: 6 */
 
 var expect = require('chai').expect;
+var mockIo = require('mock-stdio');
 
 var spawn = require('../node/spawn.js');
 
@@ -12,55 +13,17 @@ function getSpawn() {
     return spawn(bin, [arg, 'echo', 'pineapples']);
 }
 
-// allow hijacking IO, so that we can both
-// test values and not log during a test
-var fakeIo = (function () {
-
-    var originalStdout = process.stdout.write;
-    var originalStderr = process.stderr.write;
-
-    var outData = [];
-    var errData = [];
-
-    function collect(arr) {
-        return function (val) {
-            arr.push(new Buffer(val));
-        };
-    }
-
-    return {
-        activate: function () {
-            process.stdout.write = collect(outData);
-            process.stderr.write = collect(errData);
-        },
-        deactivate: function () {
-            process.stdout.write = originalStdout;
-            process.stderr.write = originalStderr;
-
-            var data = {
-                stdout: Buffer.concat(outData).toString(),
-                stderr: Buffer.concat(errData).toString()
-            };
-
-            outData = [];
-            errData = [];
-
-            return data;
-        }
-    };
-})();
-
 describe('[spawn]', () => {
 
     it('spawns real processes', function (done) {
-        fakeIo.activate();
+        mockIo.start();
 
         var proc = getSpawn();
 
         proc.on('error', done);
 
         proc.on('exit', function (code) {
-            var ioData = fakeIo.deactivate();
+            var ioData = mockIo.end();
 
             expect(code).to.equal(0);
 
@@ -72,14 +35,14 @@ describe('[spawn]', () => {
     });
 
     it('adds an error handler to the process', function (done) {
-        fakeIo.activate();
+        mockIo.start();
 
         var proc = getSpawn();
 
         expect(proc.listenerCount('error')).to.equal(1);
 
         proc.on('exit', () => {
-            fakeIo.deactivate();
+            mockIo.end();
 
             done();
         });
@@ -87,14 +50,14 @@ describe('[spawn]', () => {
     });
 
     it('adds an exit handler on the process', function (done) {
-        fakeIo.activate();
+        mockIo.start();
 
         var proc = getSpawn();
 
         expect(proc.listenerCount('exit')).to.equal(1);
 
         proc.on('exit', () => {
-            fakeIo.deactivate();
+            mockIo.end();
 
             done();
         });
@@ -102,13 +65,13 @@ describe('[spawn]', () => {
     });
 
     it('logs if an error occurs', function (done) {
-        fakeIo.activate();
+        mockIo.start();
 
         var binary = 'not-a-real-binary-i-hope';
         var proc = spawn(binary);
 
         proc.on('error', function (err) {
-            var ioData = fakeIo.deactivate();
+            var ioData = mockIo.end();
 
             expect(ioData.stdout).to.match(/shell launcher exited with error:/);
             expect(ioData.stdout).to.contain(err.message);
